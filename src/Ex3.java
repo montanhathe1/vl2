e93d5822f42311a024d8b9d23a8e596868238811
Alcasi <alvarocampelodasilva@gmail.com>
2021-07-27 16:29:43 -0300

    [ALVARO CAMPELO] Execício 3

---
+++ src/Ex3.java
@@ -0,0 +1,35 @@
+/*** 
+ * 
+ *  Agenda eletrônica
+ * -------------------
+ * 
+ * 1. Cadatro de pessoas e seu telefone
+ * 2. Campos
+ * 	2.1. Nome - String
+ * 	2.2. Telefone - String
+ * 3. Salvar em uma lista 
+ * 4. Menu - (S)alvar | (D)eletar | (C)onsultar | (S)air
+ * 
+ * Obs:
+ * 1. Salvar - inclui na lista
+ * 2. Deletar - remover da lista
+ * 3. Consultar - mostrar todas lista
+ * 4. Sair - Sai da aplicação
+ *
+ * Desenvolvimento:
+ * 1. Interface console
+ * 2. Criação a classe Agenda
+ * 3. Lista de classe Agenda
+ * 4. Não utilizar banco de dados
+ * 5. Entrega 03/08 às 9h
+ * 
+ * ***/
+
+
+public class Ex3 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
