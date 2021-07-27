f93881fdca04d324472e0830c3c2fc4e41c2b3a3
Alcasi <alvarocampelodasilva@gmail.com>
2021-07-27 14:46:45 -0300

    [ALVARO CAMPELO] Definição do segundo exercício

--- src/Ex1.java
+++ src/Ex1.java
@@ -6,44 +6,48 @@ public class Ex1 {
 
 		Scanner sc = new Scanner(System.in);
 
-		/*
-		 * System.out.print("Digite o primeiro número ou 'x' para sair:"); String
-		 * entrada = sc.next();
-		 * 
-		 * while (!"x".equals(entrada)) {
-		 * 
-		 * int num1 = Integer.parseInt(entrada);
-		 * 
-		 * System.out.print("Digite o segundo número: "); int num2 = sc.nextInt();
-		 * 
-		 * System.out.println("Resultado da soma: " + (num1 + num2));
-		 * 
-		 * System.out.print("Digite o primeiro número ou 'x' para sair:"); entrada =
-		 * sc.next();
-		 * 
-		 * }
-		 */
+		
+		 System.out.print("Digite o primeiro número ou 'x' para sair: "); 
+		 String entrada = sc.next();
+		  
+		 while (!"x".equals(entrada)) {
+		  
+			 int num1 = Integer.parseInt(entrada);
+		  
+			 System.out.print("Digite o segundo número: "); 
+			 int num2 = sc.nextInt();
+		 
+			 System.out.println("Resultado da soma: " + (num1 + num2));
+		  
+			 System.out.print("Digite o primeiro número ou 'x' para sair:"); 
+			 entrada = sc.next();
+		  
+		 }
 
-		while (true) {
+		 System.out.println("Fim");
+		 sc.close();
+		 
 
-			System.out.print("Digite o primeiro número ou 'x' para sair:");
-			String entrada = sc.next();
-
-			if ("x" == (entrada)) {
-				break;
-			}
-
-			int num1 = Integer.parseInt(entrada);
-
-			System.out.print("Digite o segundo número: ");
-			int num2 = sc.nextInt();
-
-			System.out.println("Resultado da soma: " + (num1 + num2));
-
-		}
-
-		System.out.println("Fim");
-		sc.close();
+//		while (true) {
+//
+//			System.out.print("Digite o primeiro número ou 'x' para sair:");
+//			String entrada = sc.next();
+//
+//			if ("x" == (entrada)) {
+//				break;
+//			}
+//
+//			int num1 = Integer.parseInt(entrada);
+//
+//			System.out.print("Digite o segundo número: ");
+//			int num2 = sc.nextInt();
+//
+//			System.out.println("Resultado da soma: " + (num1 + num2));
+//
+//		}
+//
+//		System.out.println("Fim");
+//		sc.close();
 
 	}
 
---
+++ src/Ex2.java
@@ -0,0 +1,33 @@
+/*** 
+ * 
+ *  Jogo do zero ou um
+ * --------------------
+ * 
+ * 1. Ao entrar no jogo, ao usuario é solicitado que escolha 0 ou 1
+ * 2. O jogo, sem saber o que o usuário escolheu, seleciona randomicamente entre 0 e 1
+ * 3. O jogo mostra o resultado e quem ganhou 
+ * 
+ * Obs:
+ * 1. Caso usuário entre com qqer caracter diferente de 0 ou 1, dar uma mensagem de erro e solicitar nova entrada
+ * 2. Para sair da aplicação, digitar "x"
+ * 3. Caso a máquina ganhe, mostrar uma messagem zoando o usuário
+ * 4. Caso o usuário ganhe, mostrar uma menssagem que foi sorte
+ * 5. Caso o usuário ganhe 3 vezes consecutivas, a máquina "rouba" e ganha a próxima
+ *
+ * Desenvolvimento:
+ * 1. Terminal ou Console
+ * 2. Deve ser criado um método para retonar a escolha da máquina
+ * 3. Para geração de números aleatórios, usar a biblioteca java.util.Random
+ * 4. Utilizar as variáveis dentro do escopo e dos tipo correto (conforme o dado)
+ * 5. Entraga: 29/07 às 9h
+ * 
+ * ***/
+
+
+public class Ex2 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
